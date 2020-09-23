091ed3120ce3c326d87cebc2a9d0fcc23b23c76f
sdkthrk <sdkthrk@gmail.com>
2020-09-23 13:02:19 +0900

    Added comment files

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,16 @@
+""" forms.py """
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
-
+    """name: PostForm """
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+    """name: CommentForm """
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
+"""models.py"""
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 
 class Post(models.Model):
+    """name: PostModel"""
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -14,5 +16,23 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    """name: CommentModel"""
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
